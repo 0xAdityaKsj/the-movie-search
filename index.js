1507eeca4373511714d7362bb30f8ceac3fe9449
Aditya <aditya.kantipudi@protonmail.com>
2023-07-17 01:37:36 +0530

    loop

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-fetch("")
+const searchEl = document.getElementById('input')
+const formEl = document.querySelector('.search-form')
+const mainEl = document.querySelector('.main')
+
+async function getData() {
+    const res = await fetch("http://www.omdbapi.com/?i=tt1630029&apikey=5a7bd67e&s=joker")
+    const data = await res.json()
+
+    console.log(data.Search)
+}
+
+
+funciton
+
+
+
+
+
+
+
+
+formEl.onsubmit = async (e) => {
+    e.preventDefault()
+    let movie = searchEl.value
+    const res = await fetch(`http://www.omdbapi.com/?i=tt1630029&apikey=5a7bd67e&s=${movie}`)
+    const data = await res.json()
+    console.log(data.Search)
+    let html = ''
+    for (item in data.Search) {
+
+        html += `<section>
+                    <div class="sec1">
+                        <img src="${data.Search[item].Poster}" alt="">
+
+                    </div>
+                    <div class="sec2">
+                        <h1>${data.Search[item].Title}</h1>
+                        <p>117min Action,drama scifi </p>
+                        <p>
+                            desc heres
+                        </p>
+                    </div>
+                </section>`
+
+    }
+
+    mainEl.innerHTML = html
+}
