4758c6265a760bad06bc7e712bc18937bd8fd49c
Aditya <aditya.kantipudi@protonmail.com>
2023-07-18 00:56:29 +0530

    new func, new params lfg

--- index.js
+++ index.js
@@ -2,22 +2,12 @@ const searchEl = document.getElementById('input')
 const formEl = document.querySelector('.search-form')
 const mainEl = document.querySelector('.main')
 
-async function getData() {
-    const res = await fetch("http://www.omdbapi.com/?i=tt1630029&apikey=5a7bd67e&s=joker")
+
+async function getData(movieId) {
+    const res = await fetch(`http://www.omdbapi.com/?apikey=5a7bd67e&i=${movieId}`)
     const data = await res.json()
-
-    console.log(data.Search)
+    return data
 }
-
-
-funciton
-
-
-
-
-
-
-
 
 formEl.onsubmit = async (e) => {
     e.preventDefault()
@@ -27,7 +17,8 @@ formEl.onsubmit = async (e) => {
     console.log(data.Search)
     let html = ''
     for (item in data.Search) {
-
+        const details = await getData(data.Search[item].imdbID)
+        const { Runtime, Genre, Plot } = details
         html += `<section>
                     <div class="sec1">
                         <img src="${data.Search[item].Poster}" alt="">
@@ -35,14 +26,14 @@ formEl.onsubmit = async (e) => {
                     </div>
                     <div class="sec2">
                         <h1>${data.Search[item].Title}</h1>
-                        <p>117min Action,drama scifi </p>
+                        <p>${Runtime},${Genre}</p>
                         <p>
-                            desc heres
+                            ${Plot}
                         </p>
                     </div>
                 </section>`
-
     }
-
     mainEl.innerHTML = html
 }
+
+
